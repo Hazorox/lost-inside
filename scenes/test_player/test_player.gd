11b83ceaecc72ab489de33c70770a4fc1f3dc162
Hamza Awad <hamzaawad8899@gmail.com>
2026-09-26 14:20:03 +0300

    Add push_direction logic
    I suffered so bad T-T

--- scenes/test_player/test_player.gd
+++ scenes/test_player/test_player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+@export var last_dir :Vector2 = Vector2.DOWN
 
 func _physics_process(delta: float) -> void:
 	var direction_y := Input.get_axis("up","down")
@@ -11,12 +11,29 @@ func _physics_process(delta: float) -> void:
 		velocity.y = direction_y * SPEED
 	else:
 		velocity.y = move_toward(velocity.y,0,SPEED)
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if direction_y <0:
+		last_dir = Vector2.UP
+	elif direction_y >0:
+		last_dir = Vector2.DOWN
+	if direction <0:
+		last_dir = Vector2.LEFT
+	elif direction>0:
+		last_dir = Vector2.RIGHT
+	
+	if Input.is_action_just_pressed("interact"):
+		var from = global_position
+		var to = global_position + last_dir * 48
+		print("from: ", from, " to: ", to)
+		var space_state = get_world_2d().direct_space_state
+		var query := PhysicsRayQueryParameters2D.create(from, to)
+		query.exclude = [self]
+		var result := space_state.intersect_ray(query)
+		print("result: ", result)
 
 	move_and_slide()
